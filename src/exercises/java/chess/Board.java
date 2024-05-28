cb64fc0e83c39146c12df9b53f8a3af250726f1c
iasmin <icb2@ic.ufal.br>
2024-05-28 09:23:38 -0300

    Adicionando método para inserir peças em um tabuleiro vazio.

--- src/exercises/java/chess/Board.java
+++ src/exercises/java/chess/Board.java
@@ -12,11 +12,7 @@ public class Board {
     private final ArrayList<Piece> pieces = new ArrayList<>();
     private final ArrayList<ArrayList<Piece>> ranks = new ArrayList<>();
     public static final StringBuilder piecesOnTheBoard = new StringBuilder();
-
     public int index = 0;
-    public Board(){
-        initialize();
-    }
 
     public void initialize() {
         this.piecesWhite = getPiecesWhite();
@@ -24,27 +20,53 @@ public class Board {
 
         addPiecesOfRank(new ArrayList<>(), Piece.Color.WHITE);
         addPiecesPawnOfRank(new ArrayList<>(), Piece.Color.WHITE);
-        addPiecesBlank();
+        addPiecesBlank(new ArrayList<>(), 4);
         addPiecesPawnOfRank(new ArrayList<>(), Piece.Color.BLACK);
         addPiecesOfRank(new ArrayList<>(), Piece.Color.BLACK);
     }
 
-//    public void addPiecesToTheBoard(){
-//        addPiecesOfRank(new ArrayList<>(), Piece.Color.WHITE);
-//        addPiecesPawnOfRank(new ArrayList<>(), Piece.Color.WHITE);
-//        addPiecesBlank();
-//        addPiecesPawnOfRank(new ArrayList<>(), Piece.Color.BLACK);
-//        addPiecesOfRank(new ArrayList<>(), Piece.Color.BLACK);
-//    }
+    public void createBoard() {
+        addPiecesBlank(new ArrayList<>(), 8);
+    }
 
-    public void addPiecesBlank() {
-        for (int x = 0; x < 4; x++) {
-            Piece blank = Piece.noPiece();
-            piecesOnTheBoard.append(String.valueOf(blank.getRepresentation()).repeat(8));
+    private void addPiecesBlank(ArrayList<Piece> aux, int rank) {
+        Piece blank = Piece.noPiece();
+        for (int x = 0; x < rank; x++){
+            for (int z = 0; z < 8; z++) {
+                aux.add(blank);
+                piecesOnTheBoard.append(blank.getRepresentation());
+            }
             piecesOnTheBoard.append(StringUtil.NEWLINE);
-            ranks.add(new ArrayList<>());
             index++;
+            ranks.add(aux);
         }
+    }
+
+    public void addPiece(Piece piece, char files, int rank){
+        int num = 0;
+        switch (files) {
+            case 'a' -> num = 0;
+            case 'b' -> num = 1;
+            case 'c' -> num = 2;
+            case 'd' -> num = 3;
+            case 'e' -> num = 4;
+            case 'f' -> num = 5;
+            case 'g' -> num = 6;
+            case 'h' -> num = 7;
+        }
+
+        ArrayList<Piece> boardRank = ranks.get(rank-1);
+        boardRank.set(num, piece);
+        pieces.add(piece);
+
+        alterPrint(piece, num, rank);
+    }
+
+    public void alterPrint(Piece piece, int files, int rank){
+        int lines = 8-rank;
+        int positionPiece = (lines*8) + files + lines;
+
+        piecesOnTheBoard.setCharAt(positionPiece, piece.getRepresentation());
     }
 
     void addPiecesOnTheBoard(ArrayList<Piece> rank, Piece.Color color){
@@ -90,6 +112,7 @@ public class Board {
         ranks.add(aux);
         addPiecesOnTheBoard(ranks.get(index), color);
     }
+
     public String print(){
         return piecesOnTheBoard.toString();
     }
@@ -127,7 +150,7 @@ public class Board {
             case 'h' -> num = 7;
         }
         ArrayList<Piece> boardRank = ranks.get(rank-1);
-        System.out.println(boardRank.get(num).getRepresentation());
+//        System.out.println(boardRank.get(num).getRepresentation());
         return boardRank.get(num).getRepresentation();
     }
 }
--- src/test/java/chess/BoardTest.java
+++ src/test/java/chess/BoardTest.java
@@ -1,4 +1,5 @@
 package chess;
+import pieces.Piece;
 import util.StringUtil;
 import junit.framework.TestCase;
 
@@ -10,6 +11,7 @@ public class BoardTest extends  TestCase{
         board = new Board();
     }
     public void testCreate(){
+        board.initialize();
         assertEquals(32, board.pieceCount());
         assertEquals(16, board.getPiecesWhite());
         assertEquals(16, board.getPiecesBlack());
@@ -48,6 +50,41 @@ public class BoardTest extends  TestCase{
     }
 
     public void testCreateBoard(){
+        board.createBoard();
 
+        assertEquals(0, board.pieceCount());
+        assertEquals(0, board.getPiecesWhite());
+        assertEquals(0, board.getPiecesBlack());
+
+        String blankRank = StringUtil.appendNewLine("........");
+        assertEquals(
+                blankRank + blankRank +
+                        blankRank + blankRank +
+                        blankRank + blankRank +
+                        blankRank + blankRank,
+                board.print()
+        );
+
+        Piece blackKing = Piece.createBlackPiece(Piece.Type.KING);
+        board.addPiece(blackKing, 'b', 6);
+        assertEquals('K', board.getPiece('b', 6));
+
+        Piece blackRook = Piece.createBlackPiece(Piece.Type.ROOK);
+        board.addPiece(blackRook, 'b', 5);
+        assertEquals('R', board.getPiece('b', 5));
+
+        Piece whiteKing = Piece.createWhitePiece(Piece.Type.KING);
+        board.addPiece(whiteKing, 'c', 4);
+        assertEquals('k', board.getPiece('c', 4));
+
+        System.out.println(board.print());
+        assertEquals(
+                blankRank + blankRank +
+                        StringUtil.appendNewLine(".K......" )+
+                        StringUtil.appendNewLine(".R......" ) +
+                        StringUtil.appendNewLine("..k....." ) +
+                        blankRank + blankRank + blankRank,
+                board.print()
+        );
     }
 }
