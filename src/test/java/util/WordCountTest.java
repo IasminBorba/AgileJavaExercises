eaff18a1cc8c1a4917bfbdcf0e7401c7670dc851
iasmin <icb2@ic.ufal.br>
2024-06-19 10:09:52 -0300

    Cria classe WordCount e seu teste.

---
+++ src/test/java/util/WordCountTest.java
@@ -0,0 +1,20 @@
+package util;
+
+import junit.framework.TestCase;
+
+public class WordCountTest extends TestCase {
+
+    public void testCreate(){
+        String text = "Create a String literal using the first two sentences of this exercise. " +
+                "You will create WordCount class to parse through the text and count the number of instances of each word.";
+
+        WordCount wordCount = new WordCount(text);
+        System.out.println(wordCount.words);
+        assertEquals(2, wordCount.count("create"));
+        assertEquals(3, wordCount.count("the"));
+        assertEquals(3, wordCount.count("of"));
+        assertEquals(1, wordCount.count("word"));
+        assertFalse(wordCount.words.containsKey("word."));
+        assertFalse(wordCount.words.containsKey("."));
+    }
+}
