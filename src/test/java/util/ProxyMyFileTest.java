dc41fa59663a7b86fc34b0050c37b4778039255c
iasmin <icb2@ic.ufal.br>
2024-07-12 14:41:50 -0300

    refatora classe ProxyMyFileTest

--- src/test/java/util/ProxyMyFileTest.java
+++ src/test/java/util/ProxyMyFileTest.java
@@ -1,11 +1,12 @@
 package util;
 
 import junit.framework.TestCase;
+import java.io.IOException;
 
 import static util.ObjectDumper.dumper;
 
 public class ProxyMyFileTest extends TestCase {
-    public void testCreate() throws IllegalAccessException {
+    public void testCreate() throws IllegalAccessException, IOException {
         String filename = "Test for class";
         MyFile myFile = new MyFile(filename);
         String toStringMyFile = dumper(myFile);
@@ -14,5 +15,14 @@ public class ProxyMyFileTest extends TestCase {
         String toStringProxyMyFile = proxyMyFile.toString();
 
         assertEquals(toStringMyFile, toStringProxyMyFile);
+
+        String text = "Modifying object";
+        myFile.write(text);
+        assertEquals(dumper(myFile), proxyMyFile.toString());
+
+        proxyMyFile.overwrite("Modifying different object");
+        assertEquals(dumper(myFile), proxyMyFile.toString());
+
+        myFile.delete();
     }
 }
